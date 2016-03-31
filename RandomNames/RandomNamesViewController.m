c38b6338bc5c5686d9c34395f36be2b2cea40861
Abdo Assem <aassem@abdos-macbook-pro.local>
2016-03-31 14:48:43 +0800

    Random Initiate

--- RandomNames/RandomNamesViewController.m
+++ RandomNames/RandomNamesViewController.m
@@ -6,22 +6,18 @@
 //  Copyright © 2016 abdoassem. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "RandomNamesViewController.h"
 
-@interface ViewController ()
+@interface RandomNamesViewController ()
 
 @end
 
-@implementation ViewController
+@implementation RandomNamesViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
