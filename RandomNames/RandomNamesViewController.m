9f6c1f02b61fa2865e7f73f5d9b8efbdfc272952
Abdo Assem <aassem@abdos-macbook-pro.local>
2016-03-31 17:04:41 +0800

    Trial Commit

--- RandomNames/RandomNamesViewController.m
+++ RandomNames/RandomNamesViewController.m
@@ -9,11 +9,41 @@
 #import "RandomNamesViewController.h"
 
 @interface RandomNamesViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *groupOne;
+@property (weak, nonatomic) IBOutlet UILabel *groupTwo;
+@property (weak, nonatomic) IBOutlet UILabel *groupThree;
+@property (weak, nonatomic) IBOutlet UITextView *outputTextView;
+@property (weak, nonatomic) IBOutlet UITextView *output2;
+@property (weak, nonatomic) IBOutlet UITextView *output3;
 
 @end
 
-@implementation RandomNamesViewController
 
+
+@implementation RandomNamesViewController
+- (IBAction)onButtonPress:(id)sender
+{
+    NSMutableArray *people = [[NSMutableArray alloc] init];
+    [people addObject:@"Abdo"];
+    [people addObject:@"Wilson"];
+    [people addObject:@"Micheal"];
+    [people addObject:@"Daniel"];
+    [people addObject:@"Ming Xiang"];
+    [people addObject:@"Jeremy"];
+    [people addObject:@"Faris"];
+    
+    for (int i = 0; i < 7; i++)
+    {
+        int n =(arc4random() % [people count]);
+        [people exchangeObjectAtIndex:i withObjectAtIndex:n];
+        self.outputTextView.text = [people componentsJoinedByString:@"\n"];
+        
+        NSString *a = [people objectAtIndex:1];
+        
+        self.output2.text = *a;
+    //    NSLog(@"%@",a);
+    }
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
